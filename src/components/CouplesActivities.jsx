d9b2db3ca4e46da8bffa4ff04f7daa0045e71c77
Marie106 <Mariebangoura404@gmail.com>
2024-08-28 03:04:06 -0400

    updated code

--- src/components/CouplesActivities.jsx
+++ src/components/CouplesActivities.jsx
@@ -6,7 +6,7 @@ const CouplesActivities = () => {
 
   return (
     <div>
-      <h1>Couples' Activities</h1>
+      <h1>Couples'Friends Activities</h1>
       <ul>
         {places.map((place) => (
           <li key={CFV7ek4ehKxvJtt8iYMu6S2hQ}>{place.name}</li>
